c2a89c9cf85020a8a0789d96d1a753bfaf3a4c72
Aninoy Mahapatra <aninoy@gmail.com>
2013-06-28 16:30:29 -0700

    Initial app scaffold commit.

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('SpreadSheetApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
