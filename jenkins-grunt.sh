86e94f1c5355e0708092ef605af3d5a494ce793a
Aninoy Mahapatra <aninoy@gmail.com>
2013-07-16 14:23:52 -0700

    Add push to stage to Jenkins script.

--- jenkins-grunt.sh
+++ jenkins-grunt.sh
@@ -1,6 +1,6 @@
 #export PATH=/usr/local/bin:/usr/local/git/bin/:/usr/local/bin/node:/usr/local/bin/phantomjs:
 
-git remote add stage_hub ~/Spreadsheet_hub.git
+git remote add stage_hub /home/aninoy/Spreadsheet_hub.git
 npm install -g grunt-cli
 npm install
 grunt build --verbose || exit 1
