99dafeb41f0322a6c6dbd67b81e9ffd6a29ce160
Aninoy Mahapatra <aninoy@gmail.com>
2013-07-16 17:06:59 -0700

    Echo messages added.

--- jenkins-grunt.sh
+++ jenkins-grunt.sh
@@ -3,6 +3,7 @@
 git remote add stage_hub /home/aninoy/Spreadsheet_hub.git
 git config --global user.name "Jenkins on Racer5"
 git config --global user.email "aninoy@gmail.com"
+git config core.sharedRepository true
 git checkout develop || exit 1
 npm install -g grunt-cli
 npm install
@@ -10,4 +11,5 @@ grunt build --verbose || exit 1
 grunt bump-only || exit 1
 git add --a || exit 1
 git commit -m 'Version bumped by Jenkins' || exit 1
+echo 'committed by jenkins'
 git push stage_hub develop || exit 1
